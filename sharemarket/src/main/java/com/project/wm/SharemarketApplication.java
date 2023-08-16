3850cca90f28c33998bed92cb52e1f23ef1bde9e
Dormailler <gerrerd@naver.com>
2023-08-16 15:05:07 +0900

    나눔게시판수정

--- sharemarket/src/main/java/com/project/wm/SharemarketApplication.java
+++ sharemarket/src/main/java/com/project/wm/SharemarketApplication.java
@@ -1,11 +1,13 @@
 package com.project.wm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"main"}) 
+@ComponentScan(basePackages = {"main","controller","dto","service"}) 
+@MapperScan(basePackages = {"dao"})
 public class SharemarketApplication {
 
 	public static void main(String[] args) {
